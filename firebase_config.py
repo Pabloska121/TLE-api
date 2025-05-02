fb1c25f4bbb96878fef716f96ef86d32f1d328bd
Pabloska121 <130798972+Pabloska121@users.noreply.github.com>
2025-05-02 18:42:36 +0200

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,15 @@
 import os
 import json
-from firebase_admin import credentials, initialize_app
+from firebase_admin import credentials, firestore, initialize_app
 
 # Obtener las credenciales de Firebase desde la variable de entorno
 firebase_credentials = json.loads(os.getenv("FIREBASE_CREDENTIALS_JSON"))
 
 # Crear las credenciales de Firebase a partir de las variables de entorno
 cred = credentials.Certificate(firebase_credentials)
+
+# Inicializar la aplicación de Firebase
 initialize_app(cred)
+
+# Crear una referencia a Firestore
+db = firestore.client()
